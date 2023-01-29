0bc5a60b53a1b6c3525a57340e6134593c230c14
HyeMin <min@Hyeui-MacBookPro.local>
2023-01-29 17:32:17 +0900

    pushswap

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 42cursus-push_swap
+
+## Mandatory
+
+- stack A 
+- stack B
+
+## Bonus
+
+- checker
---
+++ pushswap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyecheon <hyecheon@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 17:28:58 by hyecheon          #+#    #+#             */
+/*   Updated: 2023/01/29 17:30:11 by hyecheon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
